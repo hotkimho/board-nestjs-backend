1338fbd2b393d2fde6c71adf7dceec25cabdd35a
hokim <rlaghekd777@naver.com>
2022-04-30 00:09:34 +0900

    Add 회원가입, 로그인 Api Swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
-export class AuthController {}
+@ApiTags('유저 인증 API')
+export class AuthController {
+  @Post('signup')
+  @ApiTags('signup')
+  @ApiOperation({
+    summary: '회원가입 API',
+    description: '회원가입된 유저를 생성한다',
+  })
+  @ApiResponse({
+    status: 201,
+    description: '회원가입이 성공했습니다.',
+  })
+  @ApiResponse({
+    status: 409,
+    description: '회원가입이 실패했습니다.',
+  })
+  signUp(): string {
+    return 'signup code';
+  }
+
+  @Post('signin')
+  @ApiTags('signin')
+  @ApiOperation({
+    summary: '로그인 API',
+    description: '로그인을 실행한다',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '로그인이 성공했습니다.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: '로그인이 실패했습니다.',
+  })
+  signIn(): string {
+    return 'signIn code';
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Api Document')
+    .setDescription('Descript Board APIs ')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 
   if (module.hot) {
