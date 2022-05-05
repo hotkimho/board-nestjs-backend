b308cc199aab60274572053c52bb29441b5e971d
hotkimho <rlaghekd777@naver.com>
2022-05-06 06:05:10 +0900

    변동사항 없음

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
-
   if (module.hot) {
     module.hot.accept();
     module.hot.despose(() => app.close());
